872e631449ac8fe1dec22a5171f772785e385236
ecin <ecin@copypastel.com>
2012-02-20 21:23:24 -0800

    Players can now pair with a partner by sharing their session id.
    Visiting /:sessionId will attempt a pairing. This makes me lean towards
    not allowing ad-hoc pairing in a first release.
    
    TODO: don't allow players to pair with themselves.

--- server.js
+++ server.js
@@ -12,16 +12,8 @@ app.configure( function(){
   app.use(express.static(__dirname + '/public'));
 });
 
-app.get('/', function(req, res){
+app.get('/:id?', function(req, res){
   res.render('index.ejs');
-});
-
-app.get('/master', function(req, res){
-  res.render('master.ejs');
-});
-
-app.get('/slave', function(req, res){
-  res.render('slave.ejs');
 });
 
 var channels = {};
@@ -30,27 +22,51 @@ var waiting = [];
 /* When a player connects, they join the waiting FIFO queue.
  * Once we have two players, we'll automatically pair them,
  * with the older player becoming player one.
+ *
+ * Secret is to keep in mind that players are websocket
+ * connection objects.
  */
 io.sockets.on("connection", function(player){
-  util.log("Someone connected.");
-  waiting.push(player);
-  if (waiting.length >= 2) {
-    /* Pair the top two; this means our waiting queue should never
-     * grow larger than 2
-     */
-    pair(waiting.shift(), waiting.shift());
-  } else {
-    util.log("Player " + player.id + " is waiting for a partner.");
-    false;
-  }
+  util.log(player.id + " connected.");
+
+  player.on("pair", function(partnerId) {
+    // if player with partnerId is already paired, ignore request
+    // TODO: return an error
+    var partnerAlreadyPaired = getPartner(partnerId);
+    if(partnerAlreadyPaired) {
+      return;
+    }
+    util.log(player.id + " wants to pair!");
+    var partner = getPlayer(partnerId);
+    // If there is a player with the given partnerId, let's pair 'em
+    if (partner != undefined) {
+      // partner is actually player 1
+      pair(partner, player);
+    } else {
+      // couldn't find a player given a partnerId;
+      // might prefer to throw an error in this case...
+      waiting.push(player);
+      if (waiting.length >= 2) {
+        /* Pair the top two; this means our waiting queue should never
+         * grow larger than 2
+         */
+        pair(waiting.shift(), waiting.shift());
+      }
+    }
+  });
+
+  player.on("unpair", function() {
+    purge(player.id);
+  });
 
   /* After pairing, we're just acting as a proxy for messages
    * between the two players */
+
   player.on("message", function(message){
-    util.log("Got message: " + message);
-    if(channels[player.id] != undefined){
-      util.log("Sending " + message + " from " + player.id + " to " + channels[player.id].id);
-      partner = channels[player.id];
+    //util.log("Got message: " + message);
+    var partner = getPartner(player.id);
+    if(partner != undefined){
+      util.log("Sending message from " + player.id + " to " + partner.id);
       partner.send(message);
     } else {
       util.debug("Player " + player.id + " sent a message while not paired.");
@@ -60,15 +76,24 @@ io.sockets.on("connection", function(player){
 
 io.sockets.on("disconnect", function(player){
   util.log("Player "  + player.id + " has left the arcade.");
-  if( channels[conn.id] != undefined ){
-    var partner = channels[conn.id];
+  var partner = getPartner(player.id);
+  if( partner != undefined ){
     /* Let the partner know they're now alone */
     partner.send(JSON.stringify({close: player.id}));
     purge(player);
   }else{
     /* Player was in the waiting list; we need to purge him. */
+    purge(player);
   }
 });
+
+var getPlayer = function(playerId) {
+  return io.sockets.sockets[playerId];
+}
+
+var getPartner = function(playerId) {
+  return channels[playerId];
+}
 
 /* player and partner are websocket connections */
 var pair = function(player, partner) {
